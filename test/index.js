fc803b50e4662c8c0d53fd586f2864ac911e81a2
tommyfok <154274174@qq.com>
2019-01-09 16:42:44 +0800

    update test case

--- test/index.js
+++ test/index.js
@@ -7,23 +7,35 @@ let container = Runner.getContainer({
 // run is an async function
 for (let i = 0; i < 15; i++) {
     setTimeout(() => {
-        let runResult1 = container.run({
+        container.run({
             code: `console.log(myData)`,
             context: {
                 myData: i
             }
+        }).then(res1111 => {
+            console.log({res1111})
+        }).catch(e => {
+            console.log('res1111', e)
         })
-        console.log('runResult1', runResult1.message)
-        let runResult2 = container.run({
+        container.run({
             code: `console.log(2)`,
             context: {
                 myData: i
             }
+        }).then(res2222 => {
+            console.log({res2222})
+        }).catch(e => {
+            console.log('res2222', e)
         })
-        console.log('runResult2', runResult2.message)
     }, (i + 1) * 1000)
 }
 
 container.run({
     code: 'var i = 0;while(true){i++}'
+}).then(res3333 => {
+    console.log({
+        res3333
+    })
+}).catch(e => {
+    console.log('res3333', e)
 })
