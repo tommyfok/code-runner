98f8531c2875804dc835c4d525b6877b9f534d11
tommyfok <154274174@qq.com>
2019-01-10 17:07:20 +0800

    修复云函数的数据无法return到resolve handler的问题；优化代码执行机制

--- test/index.js
+++ test/index.js
@@ -8,14 +8,22 @@ let container = Runner.getContainer({
 for (let i = 0; i < 15; i++) {
     setTimeout(() => {
         container.run({
-            code: `console.log(myData)`,
+            code: `
+let p = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve('一点五秒之后result的值就是我')
+    }, 1500)
+})
+let result = await p
+// return {text:'其实我也不知道为什么'}
+`,
             context: {
                 myData: i
             }
         }).then(res1111 => {
-            console.log({res1111})
+            console.log('res1111 result:', res1111)
         }).catch(e => {
-            console.log('res1111', e)
+            console.log('res1111 error:', e)
         })
         container.run({
             code: `console.log(2)`,
@@ -23,15 +31,15 @@ for (let i = 0; i < 15; i++) {
                 myData: i
             }
         }).then(res2222 => {
-            console.log({res2222})
+            console.log('res2222 result:', res2222)
         }).catch(e => {
-            console.log('res2222', e)
+            console.log('res2222 error:', e)
         })
     }, (i + 1) * 1000)
 }
 
 container.run({
-    code: 'var i = 0;while(true){i++}'
+    code: 'var i = 0;while(true){i++};return "you are dead process"'
 }).then(res3333 => {
     console.log({
         res3333
